be2a30909576d0ad0152923e47e8afd2e2f9c3f7
Ben Greiner <bgreiner@users.noreply.github.com>
2015-09-29 20:10:07 +1000

    Pre-release housekeeping: version update, changelog

--- config/system.php
+++ config/system.php
@@ -1,7 +1,7 @@
 <?php
 // part of orsee. see orsee.org
 // THIS FILE WILL CHANGE FROM VERSION TO VERSION. BETTER NOT EDIT.
-$system__version="3.0.1";
+$system__version="3.0.2";
 
 // implemented experiment types
 $system__experiment_types=array('laboratory','online-survey','internet');
