8eab616b62714a6f644debd0b1e2d7ebcbdc320f
Daan Asscheman <d.asscheman@uu.nl>
2021-07-23 15:31:39 +0200

    Styling overgenomen van de productie.

---
+++ style/orsee_uu/html_header.php
@@ -0,0 +1,53 @@
+<div id="brandbar" class="affix-top">
+    <div class="container">
+        <div class="row">
+
+            <div class="col-sm-4 col-xs-4 logodiv">
+         <!--        <button type="button" class="navbar-toggle hidden-print" data-toggle="collapse" data-target="#main-menu-collapse">
+                    <span class="sr-only">Navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button> -->
+
+
+        <a href="
+                http://www.uu.nl/en             "><img src="/style/orsee_uu/images/uu-logo.svg" alt="Logo Utrecht University" /></a>
+            
+
+          
+            </div>
+
+            <div class="col-sm-8 col-xs-8 site-title">   
+                <h1>Department of Sociology / ICS</h1>
+                <h1>Utrecht School of Economics / TKI</h1>
+            </div>
+
+ 
+           <!--  <div class="col-md-3 col-sm-5 col-xs-2 hidden-print" role="search">
+                <div class="brandbar-search">
+                    
+                        <form method="get" id="searchform" action="http://wp.hum.uu.nl" >
+                            <label class="screen-reader-text" for="s">'</label>
+                            <input type="text" class= "searchfield" value="" name="s" id="s" placeholder="Search the Site..." />
+                            <input type="submit" id="searchsubmit" class="searchbutton" value="" />
+                        </form>
+                    
+                </div>
+            </div>
+            <div class="col-md-3 col-sm-3 col-xs-2 hidden-print">
+                <div class="language-switch"></div>
+            </div> -->
+            
+        </div>
+    </div>
+</div> 
+<div class="menu">
+    <div class="container">
+     #navigation_horizontal#
+    </div>
+</div>
+
+<div class="content">
+    <div class="container">
+        <h1>#title#</h1
