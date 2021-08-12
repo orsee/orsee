514ad0c029462e56a953d08faab8877646a0d292
Daan Asscheman <d.asscheman@uu.nl>
2021-08-12 06:53:29 +0200

    Added docker.

---
+++ docker/orsee.dockerfile
@@ -0,0 +1,56 @@
+FROM php:7.3.29-apache AS apache
+# set workdir
+RUN mkdir -p /var/www/
+WORKDIR /var/www
+
+# upgrades!
+RUN apt-get update
+RUN apt-get -y dist-upgrade
+RUN apt-get install -y dos2unix
+
+RUN apt-get install -y nano
+RUN apt-get install -y git
+RUN apt-get install -y zip unzip
+RUN apt-get install -y libxml2-dev
+RUN apt-get install -y libssh2-1
+RUN apt-get install -y libssh2-1-dev
+RUN apt-get install -y wget
+RUN apt-get install -y sudo
+RUN apt-get install -y iputils-ping
+
+RUN apt-get clean -y
+
+# set corrent TimeZone
+ENV TZ=Europe/Amsterdam
+RUN ln -snf /usr/share/zoneinfo/$TZ /etc/localtime && echo $TZ > /etc/timezone
+
+# install additional webserver packages
+RUN a2enmod ssl
+RUN a2enmod rewrite
+RUN a2enmod headers
+
+# install additional PHP extensions
+RUN docker-php-ext-install pdo_mysql mysqli soap
+
+# expose port
+EXPOSE 9004
+
+# copy httpd files
+COPY ./docker/httpd.conf /etc/apache2/sites-enabled/000-default.conf
+
+# copy webapp files
+COPY ./ /var/www
+
+# copy github token
+COPY ./docker/auth.json /root/.composer/auth.json
+
+# install self signed certifcates to thrust other local dev environments
+COPY ./docker/certificates/other-environments/myCA.pem /etc/ssl/certs
+RUN cd /usr/local/share/ca-certificates && update-ca-certificates
+
+# entrypoint
+COPY ./docker/entrypoint.sh /entrypoint.sh
+RUN chmod ugo+x /entrypoint.sh
+RUN dos2unix /entrypoint.sh
+
+ENTRYPOINT /entrypoint.sh
---
+++ install/settings-docker.php
@@ -0,0 +1,84 @@
+<?php
+// part of orsee. see orsee.org
+error_reporting(E_ALL & ~E_NOTICE);
+
+// SERVER SETTINGS
+// Web server document root, e.g. /srv/www/htdocs
+// no trailing slash!
+$settings__root_to_server="/var/www";
+
+// Experiment system root relative to server root, e.g. /orsee
+// begins always with "/" if in a subdirectory
+// no trailing slash!
+$settings__root_directory="";
+
+// url to web server document root (IP or domain name)
+// without trailing slash and the http://!
+//$settings__server_url="www.orsee.org";
+$settings__server_url="orsee.docker:9004";
+
+// servr protocol (either "http://" or "https://"
+$settings__server_protocol="https://";
+
+// Double-check your entries above! The URL to your ORSEE installation will be:
+// settings__server_protocol + settings__server_url + settings__root_directory
+
+
+// DATABASE CONFIGURATION
+// Don't forget to create the database
+$site__database_host="mariadb";
+//$site__database_port="3306"; // set only if not default 3306
+$site__database_database="orsee";
+$site__database_admin_username="orsee";
+$site__database_admin_password="secret";
+$site__database_type="mysql";
+$site__database_table_prefix="or_";
+
+// SSL mysql connection. Works with PHP >=5.3.9.
+// Use only if your database is located on a different server 
+// and you want to connect via SSL encrypted connection to it
+$site__database_use_ssl=false;
+// path name of client private key file
+$site__database_ssl_key='/etc/mysql/ssl/client-key.pem'; 
+// path name of  client public key certificate file
+$site__database_ssl_cert='/etc/mysql/ssl/client-cert.pem';
+// path name of Certificate Authority (CA) certificate file. 
+// if used, must be the same on client and server
+$site__database_ssl_ca='/etc/mysql/ssl/ca-cert.pem';
+
+// TIMEZOME SETTING
+// PHP >= 5.1.0 requires the timezone to be explicitely set.
+// If you have not set it in php.ini, then set it here. (Otherwise, you can uncomment.)
+// List of timezones: http://php.net/manual/en/timezones.php
+date_default_timezone_set("Australia/Sydney");
+
+// EMAIL MODULE
+// These settings are only needed when you plan to enable the email module
+// to retrieve emails from an external email account and process them in ORSEE
+$settings__email_server_type="pop3"; // either pop3 or imap
+$settings__email_server_name="mail.foobar.edu";
+$settings__email_server_port=""; // if empty or not set, port is automatically determined by type
+$settings__email_username="orsee@foobar.edu";
+$settings__email_password="orseefoorbar_pw";
+$settings__email_ssl=FALSE; // whether to use SSL to connect to IMAP/POP3 server (for gmail, use TRUE!)
+// E.g. for gmail, use TRUE for ssl setting. You may have to allow 
+// "Access for less secure apps" in your google account settings.
+
+
+// STOP SITE, TRACKING, DEBUGGING
+// If below is set to "y", the admin part of ORSEE won't be reachable for anybody
+// This is useful for example when running some procedures directly in the database
+$settings__stop_admin_site="n";
+
+// To stop tracking set to "y"
+$settings__disable_orsee_tracking="n";
+
+// Enable/disable debugging information output at the bottom fo each page.
+// Do NOT ENABLE on a live ORSEE system - reveals a lot of information.
+$settings__time_debugging_enabled="n";
+$settings__query_debugging_enabled="n";
+
+// Include path for tagsets. Leave as is, only change when you know what you are doing.
+ini_set("include_path",ini_get("include_path").":./tagsets:./../tagsets:./../../tagsets");
+
+?>
